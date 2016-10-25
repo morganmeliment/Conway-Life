4eacf0983a7a2a0c91fd9db928212677a34c16a8
Morgan Meliment <morganmeliment@gmail.com>
2016-10-25 14:53:37 -0400

    Update sysdeps.py

--- ggame/sysdeps.py
+++ ggame/sysdeps.py
@@ -57,7 +57,7 @@ if module_exists('browser') and module_exists('javascript'):
       self._renderer.render(self._stage)
       self._w.requestAnimationFrame(stepcallback)
       consoleText2 = document.getElementById("console").value
-      self._w.document.getElementById("textarea").innerHTML = "hello"
+      self._w.document.getElementById("textarea").value = consoleText2
       print(consoleText2)
       
     def destroy(self):
