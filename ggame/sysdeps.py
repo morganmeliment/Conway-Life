2381613307dac02d124b655800b87c385ce92e51
Morgan Meliment <morganmeliment@gmail.com>
2016-11-04 14:44:47 -0400

    Update sysdeps.py

--- ggame/sysdeps.py
+++ ggame/sysdeps.py
@@ -43,7 +43,7 @@ if module_exists('browser') and module_exists('javascript'):
       self._w.document.head.appendChild(style)
       self._w.document.body.appendChild(consoleArea)
       self._w.onunload = onclose
-      jq = window.jQuery
+      jq = self._w.jQuery
       jq("body").append('<div class = "inputScreen" style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>')
       jq(".inputScreen").hide()
   
