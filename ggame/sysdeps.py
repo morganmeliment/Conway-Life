e59bec8a8f6dbaa785226faa73b508a03975ff3d
Morgan Meliment <morganmeliment@gmail.com>
2016-11-17 14:49:45 -0500

    Update sysdeps.py

--- ggame/sysdeps.py
+++ ggame/sysdeps.py
@@ -7,74 +7,70 @@ def module_exists(module_name):
         return True
 
 if module_exists('browser') and module_exists('javascript'):
+    from browser import window, document
+    from javascript import JSObject, JSConstructor
+    try:
+        from pyinput import *
+        didLoadPyinput = True
+    except:
+        didLoadPyinput = False
+    GFX = JSObject(window.PIXI)
+    GFX_Rectangle = JSConstructor(GFX.Rectangle)
+    GFX_Texture = JSConstructor(GFX.Texture)
+    GFX_Texture_fromImage = JSConstructor(GFX.Texture.fromImage)
+    GFX_Sprite = JSConstructor(GFX.Sprite)
+    GFX_Graphics = JSConstructor(GFX.Graphics)()
+    GFX_Text = JSConstructor(GFX.Text)
+    GFX_DetectRenderer = GFX.autoDetectRenderer 
+    SND = JSObject(window.buzz)
+    SND_Sound = JSConstructor(SND.sound)
 
-  from browser import window, document
-  from javascript import JSObject, JSConstructor
-  try:
-    from pyinput import *
-    didLoadPyinput = True
-  except:
-    didLoadPyinput = False
-  
-  GFX = JSObject(window.PIXI)
-  GFX_Rectangle = JSConstructor(GFX.Rectangle)
-  GFX_Texture = JSConstructor(GFX.Texture)
-  GFX_Texture_fromImage = JSConstructor(GFX.Texture.fromImage)
-  GFX_Sprite = JSConstructor(GFX.Sprite)
-  GFX_Graphics = JSConstructor(GFX.Graphics)()
-  GFX_Text = JSConstructor(GFX.Text)
-  GFX_DetectRenderer = GFX.autoDetectRenderer 
-  SND = JSObject(window.buzz)
-  SND_Sound = JSConstructor(SND.sound)
-  
-  class GFX_Window(object):
-    
-    def __init__(self, width, height, onclose):
-      self._w = window.open("", "")
-      self._stage = JSConstructor(GFX.Container)()
-      self.width = width if width != 0 else int(window.innerWidth * 0.9)
-      self.height = height if height != 0 else int(window.innerHeight * 0.9)
-      self._renderer = GFX.autoDetectRenderer(self.width, self.height, {'transparent':True})
-      self._w.document.body.appendChild(self._renderer.view)
-      consoleArea = self._w.document.createElement('DIV')
-      self.consoleText = self._w.document.createElement('TEXTAREA')
-      self.consoleText.id = "textarea"
-      self.consoleText.readonly = True
-      consoleArea.appendChild(self.consoleText)
-      style = self._w.document.createElement('STYLE')
-      css = 'div {width: 300px; height: 100%; background-color: black; position: fixed; right: 0; top: 0;} #textarea {color: white; background-color: rgba(0,0,0,0); width: 260px; height: 810px; position: fixed; top: 20px; right: 20px; margin: 0px; resize: none; border: none; outline: none;}'
-      style.type = 'text/css'
-      style.appendChild(document.createTextNode(css))
-      self._w.document.head.appendChild(style)
-      self._w.document.body.appendChild(consoleArea)
-      self._w.onunload = onclose
-      if didLoadPyinput:
-        self._w.onload = winput_init
-      self._w.document.body.insertAdjacentHTML('beforeend', '<div id = "inputScreen" style = "display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>')
-      #self._w.document.body.innerHTML += '<div id = "inputScreen" style = "display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>'
-  
-    def bind(self, evtspec, callback):
-      self._w.document.body.bind(evtspec, callback)
-      
-    def add(self, obj):
-      self._stage.addChild(obj)
-      
-    def remove(self, obj):
-      self._stage.removeChild(obj)
-      
-    def animate(self, stepcallback):
-      self._renderer.render(self._stage)
-      self._w.requestAnimationFrame(stepcallback)
-      consoleText2 = document.getElementById("console").value
-      self._w.document.getElementById("textarea").value = consoleText2
-      
-    def destroy(self):
-      SND.all().stop()
-      self._stage.destroy()
-  
+    class GFX_Window(object):
+        
+        def __init__(self, width, height, onclose):
+            self._w = window.open("", "")
+            self._stage = JSConstructor(GFX.Container)()
+            self.width = width if width != 0 else int(window.innerWidth * 0.9)
+            self.height = height if height != 0 else int(window.innerHeight * 0.9)
+            self._renderer = GFX.autoDetectRenderer(self.width, self.height, {'transparent':True})
+            self._w.document.body.appendChild(self._renderer.view)
+            consoleArea = self._w.document.createElement('DIV')
+            self.consoleText = self._w.document.createElement('TEXTAREA')
+            self.consoleText.id = "textarea"
+            self.consoleText.readonly = True
+            consoleArea.appendChild(self.consoleText)
+            style = self._w.document.createElement('STYLE')
+            css = 'div {width: 300px; height: 100%; background-color: black; position: fixed; right: 0; top: 0;} #textarea {color: white; background-color: rgba(0,0,0,0); width: 260px; height: 810px; position: fixed; top: 20px; right: 20px; margin: 0px; resize: none; border: none; outline: none;}'
+            style.type = 'text/css'
+            style.appendChild(document.createTextNode(css))
+            self._w.document.head.appendChild(style)
+            self._w.document.body.appendChild(consoleArea)
+            self._w.onunload = onclose
+            if didLoadPyinput:
+                winput_init()
+            self._w.document.body.insertAdjacentHTML('beforeend', '<div id = "inputScreen" style = "display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>')
+            #self._w.document.body.innerHTML += '<div id = "inputScreen" style = "display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>'
+
+        def bind(self, evtspec, callback):
+            self._w.document.body.bind(evtspec, callback)
+
+        def add(self, obj):
+            self._stage.addChild(obj)
+
+        def remove(self, obj):
+            self._stage.removeChild(obj)
+
+		def animate(self, stepcallback):
+            self._renderer.render(self._stage)
+            self._w.requestAnimationFrame(stepcallback)
+            consoleText2 = document.getElementById("console").value
+            self._w.document.getElementById("textarea").value = consoleText2
+
+		def destroy(self):
+            SND.all().stop()
+            self._stage.destroy()
 
 elif module_exists('pygame'):
-
     try:
         from ggame.pygamedeps import *
     except:
@@ -85,5 +81,4 @@ else:
         from ggame.headlessdeps import *
     except:
         from headlessdeps import *
-  
-    
+
