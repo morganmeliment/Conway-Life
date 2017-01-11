51677a1983f052d3a61b12c293b11e4d2971c0aa
Morgan Meliment <morganmeliment@gmail.com>
2017-01-10 21:36:40 -0500

    Updated from Brython Server: 1/10/2017 9:37:19 PM

--- ggame/sysdeps.py
+++ ggame/sysdeps.py
@@ -50,7 +50,7 @@ if module_exists('browser') and module_exists('javascript'):
          self._w.onunload = onclose
          #if didLoadPyinput:
          #   winput_init()
-         consoleArea.insertAdjacentHTML('afterbegin', '<label class="switch"><input type="checkbox"><div class="slider round" onclick = "switched();"></div></label>')
+         consoleArea.insertAdjacentHTML('afterbegin', '<label class="switch"><input type="checkbox" onclick = "switched();"><div class="slider round"></div></label>')
          #self._w.document.body.innerHTML += '<div id = "inputScreen" style = "display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2); z-index: 50;"><div style = "position: fixed; left: calc(50% - 150px); top: calc(50% - 75px); background-color: white;"><p id = "toPrompt"></p><input type = "text" id = "toInput"><input type = "submit" id = "toSubmit"></div></div>'
          self._w.document.body.appendChild(consoleArea)
       def bind(self, evtspec, callback):
